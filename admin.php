9a6b18a45b42c302fc8ad7bb82ae26716e7090b8
111202416073-collab <111202416073@mhs.dinus.ac.id>
2026-01-15 14:32:33 +0700

    admin.php

--- admin.php
+++ admin.php
@@ -59,11 +59,15 @@ if (!isset($_SESSION['username'])) {
                     <li class="nav-item">
                         <a class="nav-link" href="admin.php?page=article">Article</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin.php?page=gallery">Gallery</a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle text-danger fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <?= $_SESSION['username'] ?>
                         </a>
                         <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="admin.php?page=profile">profile</a></li>
                             <li><a class="dropdown-item" href="logout.php">Logout</a></li>
                         </ul>
                     </li>
@@ -97,7 +101,7 @@ if (!isset($_SESSION['username'])) {
             <a href=><i class="bi bi-twitter h2 p-2 text-dark"></i></a>
             <a href=><i class="bi bi-whatsapp h2 p-2 text-dark"></i></a>
         </div>
-        <div>Lita Ayu Rahmawat &copy; 2025</div>
+        <div>Lita Ayu Rahmawati &copy; 2025</div>
     </footer>
     <!-- footer end -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
